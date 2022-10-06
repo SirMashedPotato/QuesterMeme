e355fd14b193a05f5f7881935d9d259874992b8d
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-10-07 12:55:36 +1300

    Update to 1.0.2 (initial 1.4 update)

---
+++ 1.4/Source/QuesterMeme/QuesterMeme/DefOf/HistoryEventDefOf.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+
+namespace QuesterMeme
+{
+	[DefOf]
+	public static class HistoryEventDefOf
+	{
+		static HistoryEventDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(HistoryEventDefOf));
+		}
+
+		public static HistoryEventDef QuesterMeme_QuestComplete_Success;
+		public static HistoryEventDef QuesterMeme_QuestComplete_DangerousSuccess;
+		public static HistoryEventDef QuesterMeme_QuestComplete_Fail;
+		public static HistoryEventDef QuesterMeme_QuestComplete_FailAutoAccept;
+		public static HistoryEventDef QuesterMeme_QuestExpired;
+	}
+}
---
+++ 1.4/Source/QuesterMeme/QuesterMeme/Workers/RitualAttachableOutcomeEffectWorker_GenerateQuest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Verse;
+using RimWorld;
+namespace QuesterMeme
+{
+    class RitualAttachableOutcomeEffectWorker_GenerateQuest : RitualAttachableOutcomeEffectWorker
+	{
+		public override void Apply(Dictionary<Pawn, int> totalPresence, LordJob_Ritual jobRitual, OutcomeChance outcome, out string extraOutcomeDesc, ref LookTargets letterLookTargets)
+		{
+			extraOutcomeDesc = this.def.letterInfoText;
+			GenerateQuest(jobRitual);
+			if (outcome.BestPositiveOutcome(jobRitual))
+            {
+				GenerateQuest(jobRitual);
+			}
+		}
+
+		private void GenerateQuest(LordJob_Ritual jobRitual)
+        {
+			IncidentParms parms = new IncidentParms
+			{
+				target = jobRitual.Map,
+				points = StorytellerUtility.DefaultThreatPointsNow(jobRitual.Map)
+			};
+			IncidentDefOf.GiveQuest_Random.Worker.TryExecute(parms);
+        }
+	}
+}
