18939027d35b355a61beb936e352368dc4cf69fb
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2025-06-12 13:53:41 +1200

    1.6 update

---
+++ 1.6/Source/QuesterMeme/QuesterMeme/DefOf/HistoryEventDefOf.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+
+namespace QuesterMeme
+{
+	[DefOf]
+	public static class HistoryEventDefOf
+	{
+		static HistoryEventDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(HistoryEventDefOf));
+		}
+
+		public static HistoryEventDef QuesterMeme_QuestComplete_Success;
+		public static HistoryEventDef QuesterMeme_QuestComplete_DangerousSuccess;
+		public static HistoryEventDef QuesterMeme_QuestComplete_Fail;
+		public static HistoryEventDef QuesterMeme_QuestComplete_FailAutoAccept;
+		public static HistoryEventDef QuesterMeme_QuestExpired;
+
+		public static HistoryEventDef QuesterMeme_QuestComplete_SuccessPoints;
+		public static HistoryEventDef QuesterMeme_QuestComplete_DangerousSuccessPoints;
+	}
+}
---
+++ 1.6/Source/QuesterMeme/QuesterMeme/Harmony/HarmonyInstance.cs
@@ -0,0 +1,93 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Collections.Generic;
+
+namespace QuesterMeme
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.SirMashedPotato.QuesterMeme");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void ApplyHistoryEvent(HistoryEventDef eventDef)
+        {
+            List<Pawn> playerPawns = PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_OfPlayerFaction_NoCryptosleep;
+            if (!playerPawns.NullOrEmpty())
+            {
+                foreach (Pawn p in playerPawns)
+                {
+                    HistoryEvent historyEvent = new HistoryEvent(eventDef, p.Named(HistoryEventArgsNames.Doer));
+                    Find.HistoryEventsManager.RecordEvent(historyEvent, true);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Quest))]
+        [HarmonyPatch("End")]
+        public static class Quest_End_Patch
+        {
+            [HarmonyPostfix]
+            public static void QuesterMeme_Patch(QuestEndOutcome outcome, ref Quest __instance)
+            {
+                if (!__instance.hidden && !__instance.hiddenInUI)
+                {
+                    HistoryEventDef eventDef = null;
+
+                    if (outcome == QuestEndOutcome.Success)
+                    {
+                        if (__instance.challengeRating >= 3)
+                        {
+                            eventDef = HistoryEventDefOf.QuesterMeme_QuestComplete_DangerousSuccess;
+                            IdeoUtility.Notify_HistoryEvent(new HistoryEvent(HistoryEventDefOf.QuesterMeme_QuestComplete_DangerousSuccessPoints));
+                        } 
+                        else
+                        {
+                            eventDef = HistoryEventDefOf.QuesterMeme_QuestComplete_Success;
+                            IdeoUtility.Notify_HistoryEvent(new HistoryEvent(HistoryEventDefOf.QuesterMeme_QuestComplete_SuccessPoints));
+                        }
+                    }
+                    if (outcome == QuestEndOutcome.Fail)
+                    {
+                        if (__instance.root.autoAccept)
+                        {
+                            eventDef = HistoryEventDefOf.QuesterMeme_QuestComplete_FailAutoAccept;
+                        }
+                        else
+                        {
+                            eventDef = HistoryEventDefOf.QuesterMeme_QuestComplete_Fail;
+                        }
+                    }
+                    if(!__instance.EverAccepted && __instance.State == QuestState.EndedOfferExpired)
+                    {
+                        eventDef = HistoryEventDefOf.QuesterMeme_QuestExpired;
+                    }
+
+                    if (eventDef != null)
+                    {
+                        ApplyHistoryEvent(eventDef);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Quest))]
+        [HarmonyPatch("CleanupQuestParts")]
+        public static class Quest_CleanupQuestParts_Patch
+        {
+            [HarmonyPostfix]
+            public static void QuesterMeme_Patch(ref Quest __instance)
+            {
+                if (!__instance.EverAccepted && __instance.State == QuestState.EndedOfferExpired)
+                {
+                    ApplyHistoryEvent(HistoryEventDefOf.QuesterMeme_QuestExpired);
+                }
+            }
+        }
+    }
+}
